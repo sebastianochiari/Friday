6841c8d7754ad65d66161fad70267a3d3f606a69
tommib97 <tommaso.bosetti@studenti.unitn.it>
2018-10-29 10:00:14 +0100

    cambiato nome loginServlet

--- Friday/src/main/java/it/unitn/aa1718/webprogramming/servlets/loginServlet.java
+++ Friday/src/main/java/it/unitn/aa1718/webprogramming/servlets/loginServlet.java
@@ -23,7 +23,7 @@ import java.sql.Timestamp;
  *
  * @author leo97
  */
-public class LoginServlet extends HttpServlet {
+public class loginServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
